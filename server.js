63aa5caa14375318c73f3e9c67fed07c08e1e519
Richard Edwards <richard.edwards2@bbc.co.uk>
2013-11-06 22:09:01 +0000

    Added correct port usage

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var express = require('express'),
-    app     = express();
+    app     = express(),
+    port    = process.env.PORT || 3000;
 
 app.get('/', function(request, response){
     response.send('Node Dashboard');
 });
 
-app.listen(3000);
+app.listen(port);
