4ecf62d8d341c09abcba06f97d473159471403b9
Richard Edwards <richard.edwards2@bbc.co.uk>
2013-11-18 09:32:45 +0000

    Update express output

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express'),
     port    = process.env.PORT || 3000;
 
 app.get('/', function(request, response){
-    response.send('Node Dashboard');
+    response.send('Observify');
 });
 
 app.listen(port);
